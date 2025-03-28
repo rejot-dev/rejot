7c28c56b571a830a1d4c9e6924475c9ad996b1f9
Jan Schutte <4732389+SchutteJan@users.noreply.github.com>
2025-03-28 14:15:56 -0700

    Sync: validate public schema before acknowledging

---
+++ packages/contract/schema-validator.test.ts
@@ -0,0 +1,86 @@
+import { describe, test, expect } from "bun:test";
+import { z } from "zod";
+import { zodToJsonSchema } from "zod-to-json-schema";
+import { SchemaValidator, type ISchemaLike } from "./schema-validator";
+
+describe("SchemaValidator", () => {
+  const testSchema: ISchemaLike = {
+    name: "test-schema",
+    version: {
+      major: 1,
+      minor: 0,
+    },
+    outputSchema: zodToJsonSchema(
+      z.object({
+        name: z.string(),
+        count: z.number().optional(),
+      }),
+    ),
+  };
+
+  const validator = new SchemaValidator();
+
+  test("correct object", () => {
+    const result = validator.validate(testSchema, { name: "test", count: 2 });
+    expect(result.success).toBe(true);
+  });
+
+  test("missing field", () => {
+    const result = validator.validate(testSchema, {});
+    expect(result.success).toBe(false);
+    if (!result.success) {
+      expect(result.errors[0]).toContain("required property 'name'");
+    }
+  });
+
+  test("wrong type", () => {
+    const result = validator.validate(testSchema, {
+      name: "test",
+      count: "not a number",
+    });
+    expect(result.success).toBe(false);
+    if (!result.success) {
+      expect(result.errors[0]).toContain("must be number");
+    }
+  });
+
+  test("extra fields", () => {
+    const result = validator.validate(testSchema, {
+      name: "test",
+      count: 2,
+      extra: "extra",
+    });
+    expect(result.success).toBe(false);
+    if (!result.success) {
+      expect(result.errors[0]).toContain("must NOT have additional properties");
+    }
+  });
+});
+
+describe("SchemaValidator with dates", () => {
+  const testSchema: ISchemaLike = {
+    name: "test-schema",
+    version: {
+      major: 1,
+      minor: 0,
+    },
+    outputSchema: zodToJsonSchema(
+      z.object({
+        date: z.date(),
+        datetime: z.string().datetime(),
+        time: z.string().time(),
+      }),
+    ),
+  };
+  const validator = new SchemaValidator();
+
+  test("Date, datetime, time", () => {
+    const input = {
+      date: new Date(),
+      datetime: new Date().toISOString(),
+      time: "17:20:23+00:00",
+    };
+    const result = validator.validate(testSchema, input);
+    expect(result.success).toBe(true);
+  });
+});
---
+++ packages/contract/schema-validator.ts
@@ -0,0 +1,101 @@
+import Ajv from "ajv";
+import addFormats from "ajv-formats";
+import type { JsonSchema } from "./json-schema";
+
+type ValidationResult =
+  | {
+      success: true;
+    }
+  | {
+      success: false;
+      errors: string[];
+    };
+
+export interface ISchemaLike {
+  name: string;
+  version: {
+    major: number;
+    minor: number;
+  };
+  outputSchema: JsonSchema;
+}
+
+export class SchemaValidator {
+  #ajv: Ajv;
+
+  constructor() {
+    this.#ajv = new Ajv();
+
+    // Some of the formats of ajv-formats are implemented using regexes, which is not safe to run on untrusted data.
+    // https://ajv.js.org/security.html#redos-attack
+    // Formats below are not implemented using regexes, I checked this by inspecting the source code below.
+    // (Note the version number in the URL!)
+    // https://github.com/ajv-validator/ajv-formats/blob/v3.0.1/src/formats.ts
+    addFormats(this.#ajv, [
+      "date",
+      "time",
+      "date-time",
+      "iso-time",
+      "iso-date-time",
+      "int32",
+      "int64",
+      "float",
+      "double",
+    ]);
+  }
+
+  #schemaKeyToString(schema: ISchemaLike): string {
+    return `${schema.name}:${schema.version.major}.${schema.version.minor}`;
+  }
+
+  /**
+   * Validates an object against a jsonschema.
+   * jsonschema only supports primitive types like numbers and strings, in order to validate complex types like dates,
+   * we need to serialize objects to JSON first, then run the validator.
+   *
+   * @param schemaKey - The name of the schema, used as a cache key for compiled validators
+   * @param schema - The schema to validate against.
+   * @param object - The object to validate.
+   */
+  validate(schema: ISchemaLike, object: Record<string, unknown>): ValidationResult {
+    const key = this.#schemaKeyToString(schema);
+    const validator =
+      this.#ajv.getSchema(key) ?? this.#ajv.addSchema(schema.outputSchema, key).getSchema(key);
+
+    if (!validator) {
+      throw new Error(`Schema validator not found for ${key}`);
+    }
+
+    /* 
+    The output schema we define in jsonschema for public/consumer schemas defines the schema of 
+    the serialized data, not the in-memory JavaScript object. Therefore we must serialize any objects
+    we want to validate against the schema.
+
+    Example:
+    In Postgres we have a TIMESTAMP column, after applying the public schema transformation this becomes a Date() object.
+      operation: {date: Date(2021, 1, 1)}
+    This is stored in the eventstore as a JSONB column, there the column is serialized as a ISO8601 string.
+      operation: {date: "2021-01-01T00:00:00.000Z"}
+    Our jsonschema defines this column as of type string, with a "date-time" format. This is what the validation checks:
+      validate({ type: "string", format: "date-time"}, { date: "2021-01-01T00:00:00.000Z" })
+    */
+
+    // TODO: Validate against serialized objects, don't perform the serialization here (again).
+    const result = validator(JSON.parse(JSON.stringify(object)));
+    if (!result) {
+      if (!validator.errors) {
+        throw new Error("Validation failed but no errors are reported");
+      }
+      return {
+        success: false,
+        errors: validator.errors.map(
+          (error) => `${key}${error.instancePath ?? "/"}: ${error.message ?? "unknown error"}`,
+        ),
+      };
+    }
+
+    return {
+      success: true,
+    };
+  }
+}
