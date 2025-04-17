147cf3faa88fdad1ff6d6ad5664e43b517a29feb
Jan Schutte <4732389+SchutteJan@users.noreply.github.com>
2025-04-17 10:31:40 +0200

    Manifest: add command to show jsonschema

---
+++ packages/contract-tools/manifest/manifest-jsonschema.ts
@@ -0,0 +1,19 @@
+import { zodToJsonSchema } from "zod-to-json-schema";
+import { SyncManifestSchema } from "@rejot-dev/contract/manifest";
+
+const SyncManifestJsonSchema = zodToJsonSchema(SyncManifestSchema);
+
+// Bit hacky, but it works
+const outputSchema =
+  // @ts-expect-error its a hack
+  SyncManifestJsonSchema["properties"]["publicSchemas"]["items"]["properties"]["outputSchema"];
+if (outputSchema === undefined) {
+  throw new Error("outputSchema is undefined, did the schema change?");
+}
+
+// @ts-expect-error its a hack
+SyncManifestJsonSchema["properties"]["publicSchemas"]["items"]["properties"]["outputSchema"] = {
+  $ref: "http://json-schema.org/draft-07/schema#",
+};
+
+console.log(JSON.stringify(SyncManifestJsonSchema, null, 2));
