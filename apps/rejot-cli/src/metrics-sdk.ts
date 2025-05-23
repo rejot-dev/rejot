8afb8b00595fa190ab6f7437c00d058d2fe00dee
Thies <git@thies.dev>
2025-05-23 14:06:37 +0200

    Metrics: Include current version in attributes

--- apps/rejot-cli/src/metrics-sdk.ts
+++ apps/rejot-cli/src/metrics-sdk.ts
@@ -6,6 +6,8 @@ import { PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
 import { NodeSDK } from "@opentelemetry/sdk-node";
 import { ATTR_SERVICE_NAME, ATTR_SERVICE_VERSION } from "@opentelemetry/semantic-conventions";
 
+import pkg from "../package.json" assert { type: "json" };
+
 const metricsPort = process.env["REJOT_METRICS_PORT"];
 const otlpEndpoint = process.env["OTEL_EXPORTER_OTLP_ENDPOINT"];
 
@@ -19,7 +21,7 @@ if (metricsPort || otlpEndpoint) {
   const sdk = new NodeSDK({
     resource: resourceFromAttributes({
       [ATTR_SERVICE_NAME]: "rejot-cli",
-      [ATTR_SERVICE_VERSION]: "0.1.0", // TODO: get version from package.json
+      [ATTR_SERVICE_VERSION]: pkg.version,
     }),
     traceExporter: new OTLPTraceExporter(),
     metricReader: metricsPort
