10bdc84a1d33ceed9e6c4d4d0e8712e35af06878
Thies <git@thies.dev>
2025-05-28 11:46:48 +0200

    CLI: Disable oclif automatic discovery, and use a simple plugin to load the entire list at once

--- apps/rejot-cli/src/index.ts
+++ apps/rejot-cli/src/index.ts
@@ -1,6 +1,10 @@
 import { ConsoleLogger } from "@rejot-dev/contract/logger";
 import { setLogger } from "@rejot-dev/contract/logger";
 
+import { Command, execute, Plugin } from "@oclif/core";
+import type { PJSON } from "@oclif/core/interfaces";
+
+import packagejson from "../package.json" with { type: "json" };
 import CollectCommand from "./commands/collect-command.ts";
 import { ManifestInfoCommand } from "./commands/manifest/manifest-info.command.ts";
 import { ManifestInitCommand } from "./commands/manifest/manifest-init.command.ts";
@@ -38,3 +42,59 @@ export const commands = {
 };
 
 setLogger(new ConsoleLogger("DEBUG"));
+
+// PJSON, is simply a type that oclif uses to load the package.json file.
+// We need to cast it to the PJSON type to avoid type errors, as our package.json contains the oclif field, bun just doesn't know about it.
+const pjson = packagejson as unknown as PJSON;
+
+class PreloadedRejotCli extends Plugin {
+  /**
+   * This plugin is used to preload the rejot-cli commands, so we do not have to rely on oclif's dynamic loading (which requires the package.json to be present in the root of the project).
+   * This also means we can now use bun compile to build the rejot-cli binary, and it will work out of the box.
+   */
+
+  constructor() {
+    super({
+      name: "rejot-cli",
+      root: import.meta.url,
+    });
+  }
+
+  pjson = pjson;
+  hooks = {};
+
+  commands: Command.Loadable[] = Object.values(commands).map((command) => {
+    return {
+      load: async () => {
+        return command;
+      },
+      aliases: [],
+      args: command.args,
+      flags: command.flags,
+      hidden: command.hidden,
+      hasDynamicHelp: command.hasDynamicHelp,
+      deprecateAliases: command.deprecateAliases,
+      id: command.id.replaceAll(" ", ":"),
+      hiddenAliases: command.hiddenAliases,
+      description: command.description,
+      examples: command.examples,
+    };
+  });
+}
+
+export async function run() {
+  const plugin = new PreloadedRejotCli();
+  await execute({
+    dir: import.meta.url,
+    loadOptions: {
+      isRoot: true,
+      pjson,
+      root: import.meta.url,
+      topics: plugin.topics,
+      pluginAdditions: {
+        core: [PreloadedRejotCli.name],
+      },
+      plugins: new Map([[PreloadedRejotCli.name, plugin]]),
+    },
+  });
+}
